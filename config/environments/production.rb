a7724bf39b4187410449b0d40589dd65d89a0e2d
Naomi O'Sullivan <osullivan.naomi@gmail.com>
2017-06-13 15:10:13 +1000

    background

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = true
+  config.assets.compile = true
+  # RAILS_ENV=production rake assets:precompile
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
